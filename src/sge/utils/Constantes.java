98c4e45fc04922d4076950d98d49ac404648269a
Cesar Emiliano Lezama Lopez <cesar@MacBook-Air-de-Cesar-2.local>
2023-05-23 23:10:31 -0600

    Menu principal y menu asignacion

---
+++ src/sge/utils/Constantes.java
@@ -0,0 +1,11 @@
+package sge.utils;
+
+
+
+public class Constantes {
+    
+    public static final int OPERACION_EXITOSA = 200;
+    public static final int ERROR_CONEXION = 500;
+    public static final int ERROR_CONSULTA = 501;
+    
+}
